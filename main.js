e7a233bf435b1016f5ccf0956dd1f02b40719dca
Stina <christinama92@gmail.com>
2021-01-08 11:39:51 +0000

    task 2

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ console.log("hello");
 let playerMove = "rock";
 let computerMove = "scissors";
 
+
+/*
 if (playerMove === computerMove){
     console.log("draw")
 }
@@ -30,4 +32,36 @@ if (playerMove === "rock" && computerMove === "paper"){
 if (playerMove === "paper" && computerMove === "scissors"){
     console.log("Computer Wins!")
 }
+*/
 
+function getWinner (player, computer) {
+
+    if (player === computer){
+        console.log("0")
+    }
+    
+    if (player === "rock" && computer === "scissors"){
+        console.log("1")
+    }
+    
+    if (player === "paper" && computer === "rock"){
+        console.log("1")
+    }
+    
+    if (player === "scissors" && computer === "paper"){
+        console.log("1")
+    }
+    
+    if (player === "scissors" && computer === "rock"){
+        console.log("-1")
+    }
+    
+    if (player === "rock" && computer === "paper"){
+        console.log("-1")
+    }
+    
+    if (player === "paper" && computer === "scissors"){
+        console.log("-1")
+    }
+    
+}
