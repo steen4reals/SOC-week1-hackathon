afdd3476b25d37d1dd71fc7656b04ce980332949
cavedave2 <sflanag5@gmail.com>
2021-01-08 15:58:01 +0000

    it works

--- main.js
+++ main.js
@@ -7,32 +7,28 @@ let computerMove = "scissors";
 if (playerMove === computerMove){
     console.log("draw")
 }
-
 if (playerMove === "rock" && computerMove === "scissors"){
     console.log("Player Wins!")
 }
-
 if (playerMove === "paper" && computerMove === "rock"){
     console.log("Player Wins!")
 }
-
 if (playerMove === "scissors" && computerMove === "paper"){
     console.log("Player Wins!")
 }
-
 if (playerMove === "scissors" && computerMove === "rock"){
     console.log("Computer Wins!")
 }
-
 if (playerMove === "rock" && computerMove === "paper"){
     console.log("Computer Wins!")
 }
-
 if (playerMove === "paper" && computerMove === "scissors"){
     console.log("Computer Wins!")
 }
 */
-//let computer = "rock"
+//let computer ="scissors"
+//let player = "paper"
+
 let player = prompt("choose now Rock Paper Scissors");
 console.log(player);
 
@@ -45,50 +41,37 @@ console.log(computer);
 function getWinner (player, computer) {
 
     if (player === computer){
-        //console.log("0") 
-        return "0"
-    
+        console.log(0);
+        return 0;
     }
-    
     if (player === "rock" && computer === "scissors"){
-       // console.log("1")
-        return "1"
+       console.log(1);
+        return 1;
     }
-    
-
     if (player === "paper" && computer === "rock"){
-        //console.log("1")
-        return "1"
-    }
-    
-    if (player === "scissors" && computer === "paper"){
-        //console.log("1")
-        return "1"
-    }
-    
-    if (player === "scissors" && computer === "rock"){
-       // console.log("-1")
-        return "-1"
-    }
-    
-    if (player === "rock" && computer === "paper"){
-        //console.log("-1")
-        return "-1"
-    }
-    
-    if (player === "paper" && computer === "scissors"){
-       // console.log("-1")
-        return "-1"
+        console.log(1);
+        return 1;
     } 
-
+    if (player === "scissors" && computer === "paper"){
+        console.log(1);
+        return 1;
+    }
+    if (player === "scissors" && computer === "rock"){
+        console.log(-1);
+        return -1;
+    }
+    if (player === "rock" && computer === "paper"){
+        console.log(-1);
+        return -1;
+    }
+    if (player === "paper" && computer === "scissors"){
+        console.log(-1);
+        return -1;
+    } 
 }
-let result = getWinner(computer, player);
-//console.log(result)
+//let result = getWinner(computer, player);
+let result = getWinner(player, computer);
+console.log(result)
 
 alert(result)
 
-//let compRand =["paper", "scissors", "rock"];
-
-//let computer = Math.random("paper", "scissors", "rock")
-
-//let computer = compRand[Math.floor(Math.random() * compRand.length)]
